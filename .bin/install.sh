4bf38c7e3cfa02ecaeb964aa5e7ab2db619e45d1
Bungogood <github@bungogood.com>
2022-06-11 18:30:28 +0100

    Using https instead of ssh

--- .bin/install.sh
+++ .bin/install.sh
@@ -1,4 +1,4 @@
-git clone --bare git@github.com:Bungogood/dotfiles.git $HOME/.dotfiles
+git clone --bare https://github.com/Bungogood/dotfiles.git $HOME/.dotfiles
 function config {
    /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
