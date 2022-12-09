d8e5b6e0d78d5c074ddba3a836231ef0630a106a
Bungogood <github@bungogood.com>
2022-12-09 23:57:44 +0000

    adding global ignore for ds_store

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -12,5 +12,6 @@ if [ $? = 0 ]; then
 fi;
 config checkout
 config config status.showUntrackedFiles no
+git config --global core.excludesfile ~/.gitignore_global
 rm $HOME/README.md
 rm $HOME/brewfile
